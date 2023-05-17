f06097b09a908dec2450268537600fa6ea3737c5
takuma0507 <akumatti0507@gmail.com>
2023-05-17 12:41:58 +0900

    ユーザー認証設定

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,5 @@
+class ItemsController < ApplicationController
+  def index
+    @item = Item.all
+  end
+end
---
+++ test/controllers/roots_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RootsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
