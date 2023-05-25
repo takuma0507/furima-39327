e3410e94771deeb2461721840f8286a7a634f684
takuma0507 <akumatti0507@gmail.com>
2023-05-25 22:21:47 +0900

    商品出品機能修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,14 +8,14 @@ class Item < ApplicationRecord
   belongs_to :send_region
   belongs_to :send_day
 
-
+  validates :image, presence: true
   validates :item, presence: true
   validates :item_comment, presence: true
   validates :item_category_id, presence: true
   validates :item_situation_id, presence: true
   validates :delivery_money_id, presence: true
   validates :send_region_id, presence: true
-  validates :send_day_id, presence: true
+  validates :send_day_id, presence: true, numericality: { other_than: 1 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/ } 
   validates :item_category_id, numericality: { other_than: 1 } 
   validates :item_situation_id, numericality: { other_than: 1 } 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,12 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
 
   describe '商品投稿' do
-    
-  
-  it '商品画像を１枚付けないと登録できない' do
+    context '新規登録できるとき' do
+      it '全てのカラムの値が存在すれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+context '新規登録できないとき' do
+  it '商品画像が空だと投稿できない' do
+    @item.image = nil
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Image can't be blank")
   end
 
   it '商品名が空だと投稿できない' do
@@ -75,4 +84,5 @@ RSpec.describe Item, type: :model do
     expect(@item.errors.full_messages).to include('Price is not a number')
   end
   end
+end
 end
